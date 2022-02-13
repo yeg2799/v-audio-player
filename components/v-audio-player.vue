4623ff83d9a27ade4fce2c0e07b0ae930a27d5e8
yeg2799 <yeg2799@gmail.com>
2022-02-13 18:26:11 +0300

    created button component

--- components/v-audio-player.vue
+++ components/v-audio-player.vue
@@ -1,13 +1,17 @@
 <template lang="pug">
   .v-audio-player
-    .player-button(@click="play") play
-    .stop-sound(@click="stop") stop
+    v-button(btnText="Play" @clicked="play")
+    v-button(btnText="Stop" @clicked="stop")
     audio(ref="audio" src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
 </template>
 
 <script>
 import { ref } from '@nuxtjs/composition-api';
+import VueButton from '@/components/v-button.vue';
 export default {
+  components: {
+    VueButton,
+  },
   props: {
 
   },
---
+++ components/v-button.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .button(@click="handleClick")
+    .button-text {{ btnText }}
+</template>
+
+<script>
+export default {
+  props: {
+    btnText: {
+      type: String,
+      required: true,
+      default: 'Button'
+    },
+  },
+  setup(props, { emit }) {
+    const handleClick = () => {
+      emit('clicked');
+    }
+    return {
+      handleClick,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
