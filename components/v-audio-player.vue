76a04573415353527b5039fe8c0794ddba768f4d
yeg2799 <yeg2799@gmail.com>
2022-02-13 18:17:37 +0300

    added stop and play buttons

--- components/v-audio-player.vue
+++ components/v-audio-player.vue
@@ -1,13 +1,31 @@
 <template lang="pug">
   .v-audio-player
-    audio(controls)
-      source(src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" type="audio/ogg")
-      source(src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" type="audio/mpeg")
+    .player-button(@click="play") play
+    .stop-sound(@click="stop") stop
+    audio(ref="audio" src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
 </template>
 
 <script>
+import { ref } from '@nuxtjs/composition-api';
 export default {
+  props: {
 
+  },
+  setup() {
+    const audio = ref(null);
+    const play = () => {
+      audio.value.play();
+    }
+    const stop = () => {
+      audio.value.pause();
+    }
+
+    return {
+      play,
+      audio,
+      stop,
+    }
+  }
 }
 </script>
 
